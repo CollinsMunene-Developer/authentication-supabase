7126dfd54267a4da31b27c7f76b4a83e3e74957b
CollisMunene-Developer <collinsentrepeneur@gmail.com>
2024-11-25 12:38:30 +0300

    add icons and card component implementation

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -9,14 +9,16 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import CustomForm from "./CustomForm";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
+  Email: z.string().email(),
 });
 
 interface AuthFormProps {
@@ -27,123 +29,45 @@ interface AuthFormProps {
 }
 
 const AuthForm: React.FC<AuthFormProps> = ({ type, userDetails }) => {
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        username: "",
-      },
-    })
-   
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-      // Do something with the form values.
-      // ✅ This will be type-safe and validated.
-      console.log(values)
-    }
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <section className="">
-      <div className="flex flex-col items-center align-middle justify-center bg-black-700 border-none w-full h-screen outline-none">
-        <h1 className="text-purple-500 font-bold  text-3xl ">
+      <div className="flex h-full  container flex-col items-center align-middle justify-center bg-black-700 border-none mt--16 w-full h-screen outline-none gap-2">
+        <h1 className="text-white font-bold  text-3xl ">
           {type === "signin"
-            ? `Welcome Back, ${userDetails?.firstname || "User"}!`
+            ? `Welcome Back, ${userDetails?.firstname || "User" }!`
             : "Welcome to CloudMagic"}
         </h1>
-        <p className="text-white">
+        <p className="text-gray-500">
           {type === "signin"
             ? "Sign in to your account"
             : "please sign up to continue"}
         </p>
 
-        <div className=" border-collapse bg-white w-1/2 items-center justify-center  ">
-        <Form {...form} >
-      <form onSubmit={form.handleSubmit(onSubmit)} className=  "  space-y-0 w-80 align-middle justify-center items-center  translate-x-48   ">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="ml-24">Submit</Button>
-      </form>
-    </Form>
-        </div>  
+        <div className="   w-1/2  items-center justify-center rounded-md  ">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className=" w-full align-middle justify-center items-center  translate-x-33   "
+            >
+              <CustomForm />
+            </form>
+          </Form>
+        </div>
       </div>
-
     </section>
   );
 };
---
+++ components/CustomForm.tsx
@@ -0,0 +1,209 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import { Email, Googleimg, User, Lock, Github, Microsoft } from "@/public/Icons/Icons";
+
+const CustomForm = ({ onSubmit = (data: any) => console.log(data) }) => {
+  return (
+    <Card className="w-auto max-w-2xl mx-auto">
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold text-center">
+          Profile Details
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-cols1 gap-6">
+          <div className="space-y-4">
+            <div className="grid grid-cols-3 gap-4">
+              <FormItem>
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John" className="w-full" />
+                </FormControl>
+              </FormItem>
+
+              <FormItem>
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Doe" className="w-full" />
+                </FormControl>
+              </FormItem>
+
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="collins " className="w-full" />
+                </FormControl>
+              </FormItem>
+            </div>
+
+            <FormItem>
+              <FormLabel>Phone Number</FormLabel>
+              <FormControl>
+                <div className=" flex gap-2">
+                    <Image src={User} alt="phone" width={20} height={20} />
+                <Input
+                  type="tel"
+                  placeholder="+1 (555) 000-0000"
+                  className="w-full"
+                />
+                </div>
+
+              </FormControl>
+            </FormItem>
+
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <div className=" flex gap-2">
+                    <Image src={Email} alt="email" width={20} height={20} />
+                <Input
+                  type="email"
+                  placeholder="collins.munene@shipht.it"
+                  className="w-full"
+                />
+                </div>
+
+              </FormControl>
+            </FormItem>
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <div className=" flex gap-2">
+                    <Image src={Lock} alt="lock" width={20} height={20} />
+                <Input
+                  type="password"
+                  placeholder=" Enter password" 
+                  className="w-full"
+                />
+                </div>
+
+              </FormControl>
+            </FormItem>
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+              <div className=" flex gap-2">
+                    <Image src={Lock} alt="lock" width={20} height={20} />
+                <Input
+                  type="password"
+                  placeholder=" re-enter password"
+                  className="w-full"
+                />
+                </div>
+              </FormControl>
+            </FormItem>
+          </div>
+
+          <div className="space-y-4">
+            <FormItem>
+              <FormLabel>Role</FormLabel>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select role" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="softwaredeveloper">
+                    Software Developer
+                  </SelectItem>
+                  <SelectItem value="cloudengineer">
+                    {" "}
+                    Cloud Engineer{" "}
+                  </SelectItem>
+                  <SelectItem value="frontendengineer">
+                    {" "}
+                    Solutions engineer{" "}
+                  </SelectItem>
+                  <SelectItem value="backend"> Backend engineer </SelectItem>
+                  <SelectItem value="cloud"> Cloud Archtect </SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+
+            <FormItem>
+              <FormLabel>Company</FormLabel>
+              <FormControl>
+                <Input placeholder="Company name" className="w-full" />
+              </FormControl>
+            </FormItem>
+            {/*selecting gender */}
+            <FormItem>
+              <FormLabel>Gender</FormLabel>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select gender" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="male">Male</SelectItem>
+                  <SelectItem value="female">Female</SelectItem>
+                  <SelectItem value="other">Other</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+          </div>
+        </div>
+
+        <div className="mt-6 flex justify-center">
+          <Button className="w-72" type="submit" onClick={() => onSubmit({})}>
+            Signup
+          </Button>
+        </div>
+        <FormDescription className="justify-center items-center   flex">
+          Already have an account?{" "}
+          <span className="font-bold">
+            <Link href="/sign-in">Log In</Link>
+          </span>{" "}
+        </FormDescription>
+        <span className="justify-center ml-12"> Or </span>
+
+        {/* selecting to continue with google, github or Microsoft*/}
+        <div className="flex flex-col items-center justify-center gap-2">
+          <Button  className="bg-white text-black w-72 outline-gray-800 ">
+            <Link href="#" className="flex gap-4  ">
+              {" "}
+              <Image src={Googleimg} alt="google" width={20} height={20} />{" "}
+              Continue with Google
+            </Link>
+          </Button>
+          <Button  className=" bg-white text-black outline-gray-800 w-72">
+            <Link href="#" className="flex gap-4  ">
+              {" "}
+              <Image src={Github} alt="github" width={20} height={20} />{" "}
+              Continue with Github
+            </Link>
+          </Button>
+          <Button className="  h-10 outline-black bg-white text-black w-72 ">
+            <Link href="#" className="flex gap-4">
+              {" "}
+              <Image src={Microsoft} alt="google" width={20} height={20} />{" "}
+              Continue with Microsoft
+            </Link>
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CustomForm;
